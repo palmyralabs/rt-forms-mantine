7dce5940afc38e0d0550f608aea60a889ebb3309
Agasthiya1803 <cs1803agasthiyar@gmail.com>
2024-09-02 17:53:59 +0530

    number, password field

---
+++ src/palmyra/mantine/MantineNumberField.tsx
@@ -0,0 +1,53 @@
+import { useRef, useImperativeHandle, forwardRef, MutableRefObject } from 'react';
+import { getFieldHandler, IFormFieldError, ITextField, useFieldManager, FieldDecorator } from '@palmyralabs/rt-forms';
+import { ITextFieldDefinition } from './types';
+import { getFieldLabel } from './util';
+import { NumberInput, NumberInputProps } from '@mantine/core';
+
+const MantineNumberField = forwardRef(function MantineNumberField(props: ITextFieldDefinition & NumberInputProps, ref: MutableRefObject<ITextField>) {
+    const fieldManager = useFieldManager(props.attribute, props);
+    const { getError, getValue, setValue, mutateOptions } = fieldManager;
+    const currentRef = ref ? ref : useRef<ITextField>(null);
+    const error: IFormFieldError = getError();
+    const inputRef: any = useRef(null);
+    const variant = props.variant || 'default';
+
+    useImperativeHandle(currentRef, () => {
+        const handler = getFieldHandler(fieldManager)
+        return {
+            ...handler,
+            focus() {
+                inputRef.current.focus();
+            }
+        };
+    }, [fieldManager]);
+
+    var options = fieldManager.getFieldProps();
+
+    options.onChange = (event: any) => {
+        if (!props.readOnly) {
+            setValue(event);
+            if (props.onChange)
+                props.onChange(event);
+        }
+    }
+console.log(getValue())
+    return (<>{!mutateOptions.visible &&
+        <FieldDecorator label={getFieldLabel(props)} customContainerClass={props.customContainerClass}
+            colspan={props.colspan} customFieldClass={props.customFieldClass} customLabelClass={props.customLabelClass}>
+            <NumberInput
+                defaultValue={props.defaultValue}
+                label={props.label}
+                variant={variant}
+                ref={inputRef}
+                {...options}
+                placeholder={props.placeholder}
+                value={getValue() ? getValue() : props.defaultValue}
+                error={error.message}
+            />
+        </FieldDecorator>}
+    </>
+    );
+});
+
+export { MantineNumberField };
