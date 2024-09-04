c2f10afa56ef6867e46ec46c8a76f1e8710678d0
Agasthiya1803 <cs1803agasthiyar@gmail.com>
2024-09-04 16:40:19 +0530

    slider, rating field

---
+++ src/palmyra/mantine/MantineRating.tsx
@@ -0,0 +1,69 @@
+import { useRef, useImperativeHandle, forwardRef, MutableRefObject } from 'react';
+import { IRatingDefinition } from './types';
+import { IFormFieldError, IRatingField, getFieldHandler, useFieldManager, FieldDecorator } from '@palmyralabs/rt-forms';
+import { getFieldLabel } from './util';
+import { Rating, RatingProps } from '@mantine/core';
+
+
+const MantineRating = forwardRef(function MantineRating(props: IRatingDefinition & RatingProps, ref: MutableRefObject<IRatingField>) {
+    const fieldManager = useFieldManager(props.attribute, props);
+    const { getError, getValue, setValue, mutateOptions } = fieldManager;
+    const currentRef = ref ? ref : useRef<IRatingField>(null);
+    const error: IFormFieldError = getError();
+    const inputRef = useRef(null);
+    const variant = props.variant || 'default';
+    const autoFocus = props?.autoFocus || false;
+    const fractions = props?.fractions || 1;
+    const fullSymbol: any = props?.fullSymbol;
+    const emptySymbol: any = props?.emptySymbol;
+
+    useImperativeHandle(currentRef, () => {
+        const handler = getFieldHandler(fieldManager)
+        return {
+            ...handler,
+            focus() {
+                inputRef.current.checked = true;
+                inputRef.current.focus();
+            },
+            getOptions() {
+
+            },
+            setOptions(d) {
+
+            }
+        };
+    }, [fieldManager]);
+
+
+    var options = fieldManager.getFieldProps();
+
+    options.onChange = (event: any, v: any) => {
+        if (!props.readOnly) {
+            setValue(event);
+            if (props.onChange)
+                props.onChange(event);
+        }
+    }
+
+    return (<>{!mutateOptions.visible &&
+        <FieldDecorator label={getFieldLabel(props)} customContainerClass={props.customContainerClass} colspan={props.colspan}
+            customFieldClass={props.customFieldClass} customLabelClass={props.customLabelClass}>
+            <Rating
+                variant={variant}
+                readOnly={props.readOnly}
+                inputRef={inputRef}
+                size={props.size}
+                fractions={fractions}
+                {...options}
+                fullSymbol={fullSymbol}
+                emptySymbol={emptySymbol}
+                value={getValue()}
+                error={error.message}
+                autoFocus={autoFocus}
+            />
+        </FieldDecorator>}
+    </>
+    );
+});
+
+export { MantineRating };
---
+++ src/palmyra/mantine/MantineSlider.tsx
@@ -0,0 +1,66 @@
+import { useRef, useImperativeHandle, forwardRef, MutableRefObject } from 'react';
+import { IFormFieldError, ISliderField, getFieldHandler, useFieldManager, FieldDecorator } from '@palmyralabs/rt-forms';
+import { getFieldLabel } from './util';
+import { ISliderDefinition } from './types';
+import { Slider, SliderProps } from '@mantine/core';
+
+const MantineSlider = forwardRef(function MantineSlider(props: ISliderDefinition & SliderProps, ref: MutableRefObject<ISliderField>) {
+    const fieldManager = useFieldManager(props.attribute, props);
+    const { getError, getValue, setValue, mutateOptions } = fieldManager;
+    const currentRef = ref ? ref : useRef<ISliderField>(null);
+    const error: IFormFieldError = getError();
+    const label = props.label || '';
+    const min = props.min || 0;
+    const max = props.max || 100;
+    const inputRef = useRef(null);
+
+    useImperativeHandle(currentRef, () => {
+        const handler = getFieldHandler(fieldManager)
+        return {
+            ...handler,
+            focus() {
+                inputRef.current.focus();
+            },
+            getOptions() {
+
+            },
+            setOptions(d) {
+
+            }
+        };
+    }, [fieldManager]);
+
+    var options = fieldManager.getFieldProps();
+
+    options.onChange = (d: any) => {
+        if (!props.readOnly) {
+            setValue(d);
+            if (props.onChange)
+                props.onChange(d);
+        }
+    }
+    var value = getValue() != '' ? getValue() : undefined
+
+    return (<>{!mutateOptions.visible &&
+        <FieldDecorator label={getFieldLabel(props)} customContainerClass={props.customContainerClass}
+            colspan={props.colspan}
+            customFieldClass={props.customFieldClass} customLabelClass={props.customLabelClass}>
+            <div style={{ width: '100%', textAlign: 'center' }}>
+                {label}
+                <Slider {...options}
+                    ref={inputRef}
+                    defaultValue={props.defaultValue}
+                    value={value}
+                    label={props.labelName}
+                    min={min}
+                    max={max}
+                    size={props.size}
+                    error={error.message}
+                />
+            </div>
+        </FieldDecorator>}
+    </>
+    );
+});
+
+export { MantineSlider };
