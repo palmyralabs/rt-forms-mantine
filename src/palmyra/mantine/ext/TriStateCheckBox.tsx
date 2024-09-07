d0b7cb3bf07f39e60ab2cbcd4a0c1a639009fbae
Agasthiya1803 <cs1803agasthiyar@gmail.com>
2024-09-07 15:45:59 +0530

    Tri state check box field

---
+++ src/palmyra/mantine/ext/TriStateCheckBox.tsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import { RxCross2 } from "react-icons/rx";
+import { IoCheckmarkSharp } from "react-icons/io5";
+import { Checkbox, CheckboxProps } from '@mantine/core';
+
+interface TriStateCheckBoxProps extends CheckboxProps {
+    checked?: any
+    unChecked?: any
+    // indeterminate?: any
+    color?: any
+    checkLabel?: string
+    value? : any
+}
+
+const TriStateCheckBox = (props: TriStateCheckBoxProps) => {
+
+    const triStateValue = props.value ? props.value : 0
+    const [value, setValue] = useState(triStateValue);
+
+    const CheckedIcon = props.checked || IoCheckmarkSharp
+    const UnCheckedIcon = props.unChecked || RxCross2
+    // const IndeterminateIcon = props.indeterminate || null
+
+    console.log("t", triStateValue, "v", value, "p", props.value)
+
+    const handleChange = () => {
+        setValue(v => {
+            switch (v) {
+                case 0:
+                    return 1;
+                case 1:
+                    return -1;
+                case -1:
+                default:
+                    return 0;
+            }
+        });
+    };
+
+    const CheckboxIcon = (iconProps) => {
+        switch (value) {
+            case 1:
+                return <CheckedIcon color='green' {...iconProps} />;
+            case -1:
+                return <UnCheckedIcon color='red' {...iconProps} />;
+            case 0:
+            default:
+                return null;
+        }
+    };
+
+    const checkLabel = value === 1 ? "Pass" : value === -1 ? "Fail" : "";
+
+
+    return (<>
+        <Checkbox
+            {...props}
+            icon={CheckboxIcon}
+            color={props.color || 'none'}
+            indeterminate={value === 0}
+            checked={value !== 0}
+            value={value}
+            onChange={props.onChange || handleChange}
+            disabled={props.disabled}
+            readOnly={props.readOnly}
+            label={props.checkLabel || checkLabel}
+        />
+    </>
+    );
+}
+
+export { TriStateCheckBox };
+
+export type { TriStateCheckBoxProps };
