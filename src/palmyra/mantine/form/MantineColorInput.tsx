361c148a9b2a7a4c6ee657e81cd98f738bada411
Agasthiya1803 <cs1803agasthiyar@gmail.com>
2024-09-21 17:49:44 +0530

    color input field added

---
+++ src/palmyra/mantine/form/MantineColorInput.tsx
@@ -0,0 +1,54 @@
+import { useRef, useImperativeHandle, forwardRef, MutableRefObject } from 'react';
+import { getFieldHandler, IFormFieldError, ITextField, useFieldManager, FieldDecorator } from '@palmyralabs/rt-forms';
+import { ITextFieldDefinition } from './types';
+import { getFieldLabel } from './util';
+import { ColorInput, ColorInputProps } from '@mantine/core';
+
+const MantineColorInput = forwardRef(function MantineColorInput(props: ITextFieldDefinition & ColorInputProps, ref: MutableRefObject<ITextField>) {
+    const fieldManager = useFieldManager(props.attribute, props);
+    const { getError, getValue, setValue, mutateOptions, refreshError } = fieldManager;
+    const currentRef = ref ? ref : useRef<ITextField>(null);
+    const error: IFormFieldError = getError();
+    const inputRef: any = useRef(null);
+    const variant = props.variant || 'default';
+
+    useImperativeHandle(currentRef, () => {
+        const handler = getFieldHandler(fieldManager)
+        return {
+            ...handler,
+            focus() {
+                inputRef.current.focus();
+            }
+        };
+    }, [fieldManager]);
+
+    var options = fieldManager.getFieldProps();
+
+    options.onChange = (event: any) => {
+        if (!props.readOnly) {
+            setValue(event);
+            if (props.onChange)
+                props.onChange(event);
+        }
+    }
+    options.onBlur = refreshError;
+    const value = getValue();
+
+    return (<>{!mutateOptions.visible &&
+        <FieldDecorator label={getFieldLabel(props)} customContainerClass={props.customContainerClass}
+            colspan={props.colspan} customFieldClass={props.customFieldClass} customLabelClass={props.customLabelClass}>
+            <ColorInput
+                label={props.label}
+                variant={variant}
+                ref={inputRef}
+                {...options}
+                value={value}
+                placeholder={props.placeholder}
+                error={error.message}
+            />
+        </FieldDecorator>}
+    </>
+    );
+});
+
+export { MantineColorInput };
