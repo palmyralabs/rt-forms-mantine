a3cd44efc31802591c3e507b07ef77234c17f351
surya-ponmani <gsuryaprakash2000@gmail.com>
2024-10-27 11:55:05 +0530

    numberInput field alignment change

--- src/palmyra/mantine/form/internal/NumberPickerInput.tsx
+++ src/palmyra/mantine/form/internal/NumberPickerInput.tsx
@@ -4,25 +4,43 @@ import {
 import { PickerInputBase } from './PickerInputBase';
 import { useDisclosure } from '@mantine/hooks';
 import { useState } from 'react';
+import { PiBackspaceBold } from "react-icons/pi";
+import './NumberPickerInput.css';
 
+const numberButtons = [
+    [{ label: '1', value: 1 }, { label: '2', value: 2 }, { label: '3', value: 3 }],
+    [{ label: '4', value: 4 }, { label: '5', value: 5 }, { label: '6', value: 6 }],
+    [{ label: '7', value: 7 }, { label: '8', value: 8 }, { label: '9', value: 9 }],
+    [{ label: '.', value: '.' }, { label: '0', value: 0 },
+    { label: <PiBackspaceBold className='py-number-btn-icon' />, value: 'D', isAction: true }],
+    [{ label: 'Clear All', value: 'CA', isAction: true }]
+];
 
 const NumberPickerInput = (props: NumberInputProps) => {
 
     const [value, setValue] = useState<string>('');
     const [dropdownOpened, dropdownHandlers] = useDisclosure(false)
 
-    const onClear = () => { }
+    const onClear = () => {
+        setValue('');
+    }
 
     const append = (c: string) => {
-        setValue((v) => v + c)
+        if (c === 'D') {
+            setValue((v) => v.slice(0, -1));
+        } else if (c === 'CA') {
+            onClear();
+        } else {
+            setValue((v) => v + c);
+        }
     }
 
     const NumberButton = (props: { label: string, value: number }) => {
-        return <Button onClick={() => append(props.label)}>{props.label}</Button>
+        return <Button onClick={() => append(props.label)} className='py-number-btn'>{props.label}</Button>
     }
 
-    const Action = (props:{label:string}) => {
-        return <Button>{props.label}</Button>
+    const Action = (props: { label: string, value: string }) => {
+        return <Button onClick={() => append(props.value)} className='py-number-btn'>{props.label}</Button>
     }
 
     return (
@@ -38,32 +56,18 @@ const NumberPickerInput = (props: NumberInputProps) => {
             type={'PickerInputBase'}
             __staticSelector="DatePickerInput">
 
-            <div>
-                <table>
-                    <tr>
-                        <NumberButton label='1' value={1} />
-                        <NumberButton label='2' value={2} />
-                        <NumberButton label='3' value={3} />
-                    </tr>
-
-                    <tr>
-                        <NumberButton label='4' value={4} />
-                        <NumberButton label='5' value={5} />
-                        <NumberButton label='6' value={6} />
-                    </tr>
-
-                    <tr>
-                        <NumberButton label='7' value={7} />
-                        <NumberButton label='8' value={8} />
-                        <NumberButton label='9' value={9} />
-                    </tr>
-
-                    <tr>
-                        <NumberButton label='.' value={1} />
-                        <NumberButton label='0' value={0} />
-                        <Action label='D'/>
-                    </tr>
-                </table>
+            <div className='py-number-picker-container'>
+                {numberButtons.map((row: any, rowIndex: number) => (
+                    <div key={rowIndex} className='py-number-picker'>
+                        {row.map((button: any, buttonIndex: number) => (
+                            button.isAction ? (
+                                <Action key={buttonIndex} label={button.label} value={button.value} />
+                            ) : (
+                                <NumberButton key={buttonIndex} label={button.label} value={button.value} />
+                            )
+                        ))}
+                    </div>
+                ))}
             </div>
 
         </PickerInputBase>
