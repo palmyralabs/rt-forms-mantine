7b7299059322c3c61b5e5f622f504b086ad3a01a
surya-ponmani <surya@palmyralabs.com>
2024-12-23 14:13:10 +0530

    MantineINRView Set

---
+++ src/palmyra/mantine/FormatCurrency.tsx
@@ -0,0 +1,35 @@
+import { CellGetter } from "@palmyralabs/rt-forms";
+import { CellContext, RowData } from "@tanstack/react-table";
+
+function formatCurrency(): CellGetter {
+    return (info: CellContext<RowData, any>) => {
+        let data = info.getValue();
+        if (typeof data === 'string') {
+            data = data.replace(/,/g, '');
+        }
+        data = Number(data);
+        if (!isFinite(data) || data === 0) {
+            return '0';
+        }
+        if (data !== 0) {
+            const convertAmount = data.toFixed(0);
+            const options = { minimumFractionDigits: 0, maximumFractionDigits: 0 };
+            const formattedAmount = Number(convertAmount).toLocaleString('en-IN', options);
+            return formattedAmount;
+        } else {
+            return '0';
+        }
+    };
+}
+
+function formatAmount(input: any) {
+    if (input !== 0 && input !== undefined && input !== null) {
+        const options = { minimumFractionDigits: 0, maximumFractionDigits: 0 };
+        const formattedAmount = Number(input).toLocaleString('en-IN', options);
+        return formattedAmount;
+    } else {
+        return '0';
+    }
+}
+
+export { formatCurrency, formatAmount };
---
+++ src/palmyra/mantine/form/view/MantineINRView.tsx
@@ -0,0 +1,94 @@
+import { FieldDecorator, getFieldHandler, ITextField, useFieldManager } from '@palmyralabs/rt-forms';
+import { forwardRef, MutableRefObject, useImperativeHandle, useRef, useState } from 'react';
+import { GoNumber } from "react-icons/go";
+import { MdOutlineCurrencyRupee, MdTextFields } from "react-icons/md";
+import { formatAmount } from '../../FormatCurrency';
+import { ITextFieldDefinition } from '../types';
+import { getFieldLabel } from '../util';
+import { getVariantClassName } from './variantClassName';
+import { Tooltip } from '@mantine/core';
+
+interface TextViewAttributeDefinition {
+    textAlign?: 'left' | 'right' | 'center',
+    variant?: 'standard' | 'outlined' | 'filled',
+    valueFormat?: 'number' | 'text';
+}
+
+const MantineINRView = forwardRef(function MantineTextView(props: ITextFieldDefinition & TextViewAttributeDefinition,
+    ref: MutableRefObject<ITextField>) {
+
+    const fieldManager = useFieldManager(props.attribute, props);
+    const { getValue, mutateOptions } = fieldManager;
+    const currentRef = ref ? ref : useRef<ITextField>(null);
+    const textAlign: any = props.textAlign || 'left';
+    const inputRef: any = useRef(null);
+    const variant = props.variant || 'standard';
+    const valueFormat = props.valueFormat || 'number';
+
+    useImperativeHandle(currentRef, () => {
+        const handler = getFieldHandler(fieldManager)
+        return {
+            ...handler,
+            focus() {
+                inputRef.current.focus();
+            }
+        };
+    }, [fieldManager]);
+
+    const [showInWords, setShowInWords] = useState(valueFormat === 'text' ? true : false);
+    const value = getValue();
+
+    const toggleView = () => {
+        setShowInWords(!showInWords);
+    };
+    const formatAmountText = (input: any) => {
+        if (input >= 10000000) {
+            const crores = (input / 10000000).toFixed(2);
+            const cr = Number(crores);
+            return `${crores} Crore${cr > 1 ? 's' : ''}`;
+        } else if (input >= 100000) {
+            const lakhs = (input / 100000).toFixed(2);
+            const lak = Number(lakhs);
+            return `${lakhs} Lakh${lak > 1 ? 's' : ''}`;
+        }
+        return formatAmount(input);
+    };
+
+    var options = fieldManager.getFieldProps();
+
+    return (<>{!mutateOptions.visible &&
+        <FieldDecorator label={getFieldLabel(props)} customContainerClass={props.customContainerClass} colspan={props.colspan}
+            customFieldClass={props.customFieldClass} customLabelClass={props.customLabelClass}>
+            {(props.label) ?
+                <div {...options} className='text-view-field-container'>
+                    <div className="text-view-label">{props.label}</div>
+                    <div className={getVariantClassName(variant, props.label)}>
+                        <MdOutlineCurrencyRupee />
+                        {formatAmount(getValue())}
+                    </div>
+                </div> :
+                <div {...options} style={{ textAlign: textAlign }}>
+                    <div className={getVariantClassName(variant, props.title)} style={{
+                        display: 'flex', alignItems: 'center', justifyContent: 'space-between'
+                    }}>
+                        <div style={{
+                            display: 'flex', alignItems: 'center'
+                        }}>
+                            <MdOutlineCurrencyRupee />
+                            {showInWords ? formatAmountText(value) : formatAmount(value)}
+                        </div>
+                        <Tooltip label={showInWords ? 'Switch to Digits' :'Switch to Words'} withArrow>
+                            <div onClick={toggleView} style={{ cursor: 'pointer' }}>
+                                {showInWords ? <GoNumber /> : <MdTextFields />}
+                            </div>
+                        </Tooltip>
+                    </div>
+
+                </div>
+            }
+        </FieldDecorator>}
+    </>
+    );
+});
+
+export { MantineINRView };
