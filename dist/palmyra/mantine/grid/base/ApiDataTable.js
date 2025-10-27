70c22d6f6056c9a6eae05319a4366fb8bb5b3974
Agasthiya1803 <agasthiya@palmyralabs.com>
2025-10-27 15:41:07 +0530

    latest build

--- dist/palmyra/mantine/grid/base/ApiDataTable.js
+++ dist/palmyra/mantine/grid/base/ApiDataTable.js
@@ -1,11 +1,11 @@
-import { jsx as h } from "react/jsx-runtime";
-import { NoopGridCustomizer as y, useServerQuery as k, generateColumns as z, EmptyChildTable as Q } from "@palmyralabs/rt-forms";
-import { forwardRef as R, useRef as v, useImperativeHandle as d } from "react";
-import w from "./BaseTable.js";
-import { useLSQueryOptions as x } from "./useLSQueryOptions.js";
-const G = R(function(s, u) {
-  const { columns: m, EmptyChild: l, lsKey: r } = s, g = l || Q, n = s.customizer || y, o = x({ lsKey: r, pageSize: s.pageSize }), P = { ...s.initParams, ...o.getLSOptions() }, a = { ...s, initParams: P }, t = k(a), i = u || v(null);
-  d(i, () => r ? { ...t, setQuickSearch: (e) => {
+import { jsx as p } from "react/jsx-runtime";
+import { NoopGridCustomizer as y, useServerQuery as k, generateColumns as z, EmptyChildTable as w } from "@palmyralabs/rt-forms";
+import { forwardRef as Q, useRef as R, useImperativeHandle as v } from "react";
+import F from "./BaseTable.js";
+import { useLSQueryOptions as d } from "./useLSQueryOptions.js";
+const G = Q(function(s, u) {
+  const { columns: m, EmptyChild: l, lsKey: r } = s, g = l || w, n = s.customizer || y, o = d({ lsKey: r, pageSize: s.pageSize }), P = { ...s.initParams, ...o.getLSOptions() }, a = { ...s, initParams: P }, t = k(a), i = u || R(null);
+  v(i, () => r ? { ...t, setQuickSearch: (e) => {
     o.setQuickSearch(e), t.setQuickSearch(e);
   }, setFilter: (e) => {
     o.setFilter(e), t.setFilter(e);
@@ -25,16 +25,17 @@ const G = R(function(s, u) {
   const S = z(m, n), C = s.onRowClick ? (c) => {
     s.onRowClick(c);
   } : () => {
-  }, f = t.getCurrentData(), p = i.current?.setSortColumns || t.setSortColumns;
-  return /* @__PURE__ */ h(
-    w,
+  }, f = t.getCurrentData(), h = i.current?.setSortColumns || t.setSortColumns;
+  return /* @__PURE__ */ p(
+    F,
     {
       columnDefs: S,
       EmptyChild: g,
       customizer: n,
+      showFooter: s.showFooter,
       rowData: f,
       onRowClick: C,
-      onColumnSort: p,
+      onColumnSort: h,
       initParams: a.initParams
     }
   );
