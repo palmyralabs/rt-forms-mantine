8ec36f3d6b8646ec848ad904a8930e5c96f8d4cf
Agasthiya1803 <agasthiya@palmyralabs.com>
2025-11-26 16:07:32 +0530

    latest build

--- dist/palmyra/mantine/grid/plugins/pagination/SelectablePagination.js
+++ dist/palmyra/mantine/grid/plugins/pagination/SelectablePagination.js
@@ -2,21 +2,21 @@ import { jsx as e, jsxs as o } from "react/jsx-runtime";
 import { Select as w, Pagination as x } from "@mantine/core";
 import { delayGenerator as z } from "@palmyralabs/ts-utils";
 import { forwardRef as C, useCallback as I, useState as u, useRef as j, useImperativeHandle as k } from "react";
-import '../../../../../assets/SelectablePagination.css';const D = C(function(n, c) {
-  const h = I(z(50), []), a = n.queryRef?.current, [A, m] = u(0), [E, f] = u(), g = c || j(null);
+import '../../../../../assets/SelectablePagination.css';const D = C(function(a, c) {
+  const h = I(z(50), []), n = a.queryRef?.current, [A, m] = u(0), [E, v] = u(), g = c || j(null);
   k(g, () => ({
     refresh() {
       h(() => m((t) => t + 1));
     }
   }), [g]);
-  const v = (t, p) => {
-    const N = parseInt(p.value, 10);
-    f(p.value), a.setPageSize(N);
-  }, s = a?.getTotalRecords() || 0, y = a?.getQueryLimit() || { limit: 15 }, i = Array.isArray(n.pageSize) ? n.pageSize : [n.pageSize], d = a?.getPageNo() || 0, l = y.limit || 25, r = Math.ceil(s / l), P = d * l + 1, S = Math.min((d + 1) * l, s), b = (t) => {
-    a.gotoPage(t - 1);
-  }, R = i.map((t) => t);
+  const P = (t, p) => {
+    const R = parseInt(p.value, 10);
+    v(p.value), n.setPageSize(R);
+  }, s = n?.getTotalRecords() || 0, f = n?.getQueryLimit() || { limit: 15 }, i = Array.isArray(a.pageSize) ? a.pageSize : [a.pageSize], d = n?.getPageNo() || 0, l = f.limit || 25, r = Math.ceil(s / l), y = d * l + 1, S = Math.min((d + 1) * l, s), b = (t) => {
+    n.gotoPage(t - 1);
+  }, N = i.map((t) => t);
   return /* @__PURE__ */ e("div", { children: (() => {
-    const t = n.ignoreSinglePage ? 1 : 0;
+    const t = a.ignoreSinglePage ? 1 : 0;
     return !isNaN(r) && r > t;
   })() && /* @__PURE__ */ e("div", { children: /* @__PURE__ */ o("div", { className: "py-selectable-pagination-container", children: [
     /* @__PURE__ */ e("div", { className: "py-selectable-pagination-left-container", children: i && i.length > 1 ? /* @__PURE__ */ o("div", { className: "py-selectable-pagination-left-content-container", children: [
@@ -26,14 +26,14 @@ import '../../../../../assets/SelectablePagination.css';const D = C(function(n, 
         {
           id: "rows-per-page-select",
           defaultValue: i[0].toString(),
-          onChange: v,
+          onChange: P,
           size: "xs",
           checkIconPosition: "right",
-          data: R.toString().split(",")
+          data: N.toString().split(",")
         }
       ) }) }),
       /* @__PURE__ */ e("div", { className: "py-selectable-pagination-show-result", children: /* @__PURE__ */ o("span", { children: [
-        P,
+        y,
         " - ",
         S,
         " of ",
@@ -45,7 +45,8 @@ import '../../../../../assets/SelectablePagination.css';const D = C(function(n, 
       x,
       {
         total: r,
-        onChange: b
+        onChange: b,
+        value: n.getPageNo() + 1
       }
     ) })
   ] }) }) });
